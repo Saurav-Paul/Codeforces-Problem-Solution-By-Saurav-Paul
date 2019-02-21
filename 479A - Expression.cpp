4db1ecdd84c6f74c914def3ca84ca40fbbb792d1
Saurav Paul <35382323+Saurav-Paul@users.noreply.github.com>
2019-02-22 04:29:20 +0600

    Create 479A - Expression.cpp

---
+++ 479A - Expression.cpp
@@ -0,0 +1,67 @@
+#include<bits/stdc++.h>
+#define nl '\n'
+#define _time_ clock_t tStart = clock();
+#define _show_ printf("Time taken: %.6fs\n", (double)(clock() - tStart)/CLOCKS_PER_SEC);
+#define ll long long int
+#define loop(a,b)           for(int i=a;i<=b;++i)
+#define count_1(n)            __builtin_popcountll(n)
+#define pb                    push_back
+#define F                     first
+#define S                     second
+#define mp                    make_pair
+#define clr(x)                x.clear()
+#define MOD                   1000000007
+#define itoc(c)               ((char)(((int)'0')+c))
+#define vi                    vector<int>
+#define pll                   pair<ll,ll>
+#define pii                   pair<int,int>
+#define all(p)                p.begin(),p.end()
+#define mid(s,e)              (s+(e-s)/2)
+#define sv()                  ll t,n; scanf("%lld",&t);n=t; while(t--)
+#define tcase()               ll t,n; cin>>t;n=t; while(t--)
+#define iscn(num)             scanf("%d",&num);
+
+using namespace std;
+void FAST_IO();
+int main()
+{
+    //    _time_
+   FAST_IO();
+   int a,b,c;
+   cin>>a>>b>>c;
+    int ans=0,temp;
+    temp=a+(b*c);
+    ans=max(ans,temp);
+
+    temp=a*(b+c);
+    ans=max(ans,temp);
+
+    temp=a*(b*c);
+    ans=max(ans,temp);
+
+    temp=a+(b+c);
+    ans=max(ans,temp);
+
+    temp=(a*b)+c;
+    ans=max(ans,temp);
+
+     temp=(a+b)*c;
+    ans=max(ans,temp);
+
+  cout<<ans<<endl;
+
+//    _show_
+    return 0;
+}
+void FAST_IO()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    //cout.setf(ios::fixed);
+    //cout.precision(20);
+#ifndef _offline
+    //freopen("input.txt", "r", stdin);
+    //freopen("output.txt", "w", stdout);
+#endif
+}

af737b7e774dc1c0b61dc5b61e14b3ef63548849
Saurav Paul <35382323+Saurav-Paul@users.noreply.github.com>
2019-03-05 14:51:45 +0600

    Implementation

---
+++ 978B - File Name.cpp
@@ -0,0 +1,69 @@
+#include<bits/stdc++.h>
+#define endl '\n'
+#define ll long long int
+#define loop(a,b)             for(int i=a;i<=b;++i)
+#define pb                    push_back
+#define F                     first
+#define S                     second
+#define mp                    make_pair
+#define clr(x)                x.clear()
+#define MOD                   1000000007
+#define itoc(c)               ((char)(((int)'0')+c))
+#define vi                    vector<int>
+#define all(p)                p.begin(),p.end()
+#define mid(s,e)              (s+(e-s)/2)
+#define sv()                  ll t,n; scanf("%lld",&t);n=t; while(t--)
+#define tcase()               ll t,n; cin>>t;n=t; while(t--)
+#define iscn(num)             scanf("%d",&num);
+using namespace std;
+bool file=0,rt=1;
+clock_t tStart ;
+void FAST_IO();
+////////////////////////
+
+int main()
+{
+
+    FAST_IO();
+    ////////////////////////
+    
+    ll n;
+    cin>>n;
+    string s;
+    cin>>s;
+    ll sz=s.size();
+    bool check=0;
+    ll ans=0;
+    for(int i=1;i<sz;i++){
+        if(check && s[i]=='x')
+            ans++;
+        if(s[i-1]=='x' && s[i]=='x')
+            check =1;
+        if(s[i]!='x')
+            check =0;
+
+
+    }
+    cout<<ans<<endl;
+    ////////////////////////
+    if(rt && file){
+     printf("\nTime taken: %.6fs\n", (double)(clock() - tStart)/CLOCKS_PER_SEC);
+    }
+    return 0;
+   }
+
+void FAST_IO()
+{
+    ios_base::sync_with_stdio(0);
+    //cin.tie(NULL);
+    //cout.tie(NULL);
+    //cout.setf(ios::fixed);
+    //cout.precision(2);
+    if(rt && file){ tStart = clock(); }
+    if(file){
+    #ifndef _offline
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+    }
+}

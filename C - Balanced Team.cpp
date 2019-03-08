fdc41e408db2ebdbd0a8c1cd596c0f96978e0005
Saurav Paul <35382323+Saurav-Paul@users.noreply.github.com>
2019-03-08 13:32:00 +0600

    Greedy

---
+++ C - Balanced Team.cpp
@@ -0,0 +1,74 @@
+#include<bits/stdc++.h>
+#define endl '\n'
+#define ll long long int
+#define loop(i,a,b)           for(ll i=a;i<=b;++i)
+#define pb                    push_back
+#define F                     first
+#define S                     second
+#define mp                    make_pair
+#define clr(x)                x.clear()
+#define MOD                   1000000007
+#define itoc(c)               ((char)(((int)'0')+c))
+#define vi                    vector<int>
+#define all(p)                p.begin(),p.end()
+#define mid(s,e)              (s+(e-s)/2)
+#define sv()                  ll t,n; scanf("%lld",&t);n=t; while(t--)
+#define tcase()               ll t,n; cin>>t;n=t; while(t--)
+#define iscn(num)             scanf("%d",&num);
+using namespace std;
+bool file=0,rt=1;
+clock_t tStart ;
+void FAST_IO();
+////////////////////////
+
+int main()
+{
+
+    FAST_IO();
+    ////////////////////////
+    ll n;
+    cin>> n;
+    std::vector<ll> vec;
+    loop(i,1,n){
+        ll temp;
+        cin>> temp;
+        vec.pb(temp);
+    }
+    sort(all(vec));
+    ll sz=vec.size();
+    ll ans=1;
+    for(ll i=0; i<= sz-ans ; i++){
+        ll cnt=ans;
+        if(vec[i+ans]-vec[i]<=5){
+            for(ll j=i+ans ; j< sz ; j++){
+                if(vec[j]-vec[i]<=5)
+                    cnt++;
+                else
+                    break ;
+            }
+        }
+        ans=max(ans, cnt);
+    }
+    cout<<ans<<endl;
+    ////////////////////////
+    if(rt && file){
+     printf("\nTime taken: %.6fs", (double)(clock() - tStart)/CLOCKS_PER_SEC);
+    }
+    return 0;
+   }
+
+void FAST_IO()
+{
+    ios_base::sync_with_stdio(0);
+    //cin.tie(NULL);
+    //cout.tie(NULL);
+    //cout.setf(ios::fixed);
+    //cout.precision(2);
+    if(rt && file){ tStart = clock(); }
+    if(file){
+    #ifndef _offline
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+    }
+}

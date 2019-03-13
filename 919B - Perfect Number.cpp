415d4a99fbfd6ed87f048fc6e5f5b8a7049f2c02
Saurav Paul <35382323+Saurav-Paul@users.noreply.github.com>
2019-03-13 21:47:00 +0600

    Dp Brute_force

---
+++ 919B - Perfect Number.cpp
@@ -0,0 +1,65 @@
+#include<bits/stdc++.h>
+#define endl '\n'
+#define ll long long int
+#define loop(i,a,b)           for(ll i=a;i<=b;++i)
+#define pb                    push_back
+#define F                     first
+#define S                     second
+#define mp                    make_pair
+#define clr(x)                x.clear()
+#define MOD                   1000000007
+#define itoc(c)               ((char)(((int)'0')+c))
+#define vl                    vector<ll>
+#define SZ(x)                 (x).size()
+#define all(p)                p.begin(),p.end()
+#define mid(s,e)              (s+(e-s)/2)
+#define sv()                  ll t,n; scanf("%lld",&t);n=t; while(t--)
+#define tcase()               ll t,n; cin>>t;n=t; while(t--)
+#define iscn(num)             scanf("%d",&num);
+using namespace std;
+bool file=0,rt=1;
+clock_t tStart ;
+void FAST_IO();
+////////////////////////
+ll mem[22222000];
+ll check(ll n){
+    if(n==0)
+        return 0;
+    if(mem[n])
+        return mem[n];
+    return mem[n]=check(n/10)+n%10;
+}
+int main()
+{
+
+    FAST_IO();
+    ////////////////////////
+    ll n,ans;
+    cin>> n;
+    for(int i=19 ; n ; i++){
+        if(check(i)==10)
+            n--,ans=i;
+    }
+    cout<<ans<<endl;
+    ////////////////////////
+    if(rt && file){
+     printf("\nTime taken: %.6fs", (double)(clock() - tStart)/CLOCKS_PER_SEC);
+    }
+    return 0;
+   }
+
+void FAST_IO()
+{
+    ios_base::sync_with_stdio(0);
+    //cin.tie(NULL);
+    //cout.tie(NULL);
+    //cout.setf(ios::fixed);
+    //cout.precision(2);
+    if(rt && file){ tStart = clock(); }
+    if(file){
+    #ifndef _offline
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+    }
+}

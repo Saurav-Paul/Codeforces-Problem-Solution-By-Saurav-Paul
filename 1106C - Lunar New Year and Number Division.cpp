d1dd216c0a136e936dda42cd34f506f7bc3ff4f4
Saurav Paul <35382323+Saurav-Paul@users.noreply.github.com>
2019-03-15 23:26:33 +0600

    Greedy

---
+++ 1106C - Lunar New Year and Number Division.cpp
@@ -0,0 +1,62 @@
+#include<bits/stdc++.h>
+#define endl '\n'
+#define ll long long int
+#define loop(i,a,b)           for(ll i=a;i<=b;++i)
+#define pb                    push_back
+#define F                     first
+#define S                     second
+#define mp                    make_pair
+#define clr(x)                x.clear()
+#define MOD                   1000000007
+#define itoc(c)               ((char)(((int)'0')+c))
+#define vl                    vector<ll>
+#define SZ(x)                 (x).size()
+#define all(p)                p.begin(),p.end()
+#define mid(s,e)              (s+(e-s)/2)
+#define sv()                  ll t,n; scanf("%lld",&t);n=t; while(t--)
+#define tcase()               ll t,n; cin>>t;n=t; while(t--)
+#define iscn(num)             scanf("%d",&num);
+using namespace std;
+bool file=0,rt=1;
+clock_t tStart ;
+void FAST_IO();
+////////////////////////
+
+int main()
+{
+
+    FAST_IO();
+    ////////////////////////
+    ll n,temp;
+    cin>>n;
+    std::vector<ll> vec;
+    for(int i=0; i< n ; i++) 
+        cin>>temp,vec.pb(temp);
+    sort(all(vec));
+    ll ans=0;
+    ll sz=SZ(vec);
+    for(int i=0,j=sz-1; i<j ; i++,j--)
+        ans+=( (vec[i]+vec[j])*(vec[i]+vec[j]));
+    cout<<ans<<endl;
+    ////////////////////////
+    if(rt && file){
+     printf("\nTime taken: %.6fs", (double)(clock() - tStart)/CLOCKS_PER_SEC);
+    }
+    return 0;
+   }
+
+void FAST_IO()
+{
+    ios_base::sync_with_stdio(0);
+    //cin.tie(NULL);
+    //cout.tie(NULL);
+    //cout.setf(ios::fixed);
+    //cout.precision(2);
+    if(rt && file){ tStart = clock(); }
+    if(file){
+    #ifndef _offline
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+    }
+}

5ae50de07bd7eb86d2e9c0188571bccf0902ac91
Saurav Paul <35382323+Saurav-Paul@users.noreply.github.com>
2019-03-18 23:15:17 +0600

    Implementation

---
+++ 1104A - Splitting into digits.cpp
@@ -0,0 +1,56 @@
+#include<bits/stdc++.h>
+#define endl '\n'
+#define ll long long int
+#define loop(i,a,b)           for(ll i=a;i<=b;++i)
+#define pb                    push_back
+#define F                     first
+#define S                     second
+#define mp                    make_pair
+#define clr(x)                x.clear()
+#define MOD                   1000000007
+#define itoc(c)               ((char)(((int)'0')+c))
+#define SZ(x)                 (x).size()
+#define all(p)                p.begin(),p.end()
+#define mid(s,e)              (s+(e-s)/2)
+#define tcase()               ll t,n; cin>>t;n=t; while(t--)
+#define iscn(num)             scanf("%d",&num);
+using namespace std;
+typedef pair<ll,ll>Pair;
+bool file=0,rt=1;
+clock_t tStart ;
+void FAST_IO();
+////////////////////////
+
+int main()
+{
+
+    FAST_IO();
+    ////////////////////////
+    ll n;
+    cin>>n;
+    cout<<n<<endl;
+    for(int i=0; i< n ; i++)
+        cout<<1<<" ";
+    cout<<endl;
+    ////////////////////////
+    if(rt && file){
+     printf("\nTime taken: %.6fs", (double)(clock() - tStart)/CLOCKS_PER_SEC);
+    }
+    return 0;
+   }
+
+void FAST_IO()
+{
+    ios_base::sync_with_stdio(0);
+    //cin.tie(NULL);
+    //cout.tie(NULL);
+    //cout.setf(ios::fixed);
+    //cout.precision(2);
+    if(rt && file){ tStart = clock(); }
+    if(file){
+    #ifndef _offline
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+    }
+}

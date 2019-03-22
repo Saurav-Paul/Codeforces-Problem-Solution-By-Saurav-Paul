90d0a29254ff47de7b4f14e4c41eb7e4476c7e17
Saurav Paul <35382323+Saurav-Paul@users.noreply.github.com>
2019-03-22 19:07:35 +0600

    Greedy Math

---
+++ 158B - Taxi.cpp
@@ -0,0 +1,69 @@
+#include<bits/stdc++.h>
+#define endl '\n'
+#define ll long long int
+#define loop(i,a,b)           for(ll i=a;i<=b;++i)
+#define pb                    push_back
+#define F                     first
+#define S                     second
+#define mp                    make_pair
+#define clr(x)                x.clear()
+#define MOD                   1000000007
+#define itoc(c)               ((char)(((int)'0')+c))
+#define vl                    vector<ll>
+#define SZ(x)                 (x).size()
+#define all(p)                p.begin(),p.end()
+#define mid(s,e)              (s+(e-s)/2)
+#define sv()                  ll t,n; scanf("%lld",&t);n=t; while(t--)
+#define tcase()               ll t,n; cin>>t;n=t; while(t--)
+#define iscn(num)             scanf("%d",&num);
+using namespace std;
+typedef pair<ll,ll> Pair;
+bool file=0,rt=1;
+clock_t tStart ;
+void FAST_IO();
+////////////////////////
+int people[4];
+int main()
+{
+
+    FAST_IO();
+    ////////////////////////
+    ll n;
+    cin>>n;
+    
+    for(int i=0; i< n ; i++){
+        ll temp;
+        cin>>temp;
+        people[temp]++;
+    }
+    ll cnt=0;
+    cnt+=people[4];
+    cnt+=people[3];
+    people[1]=max(0,people[1]-people[3]);
+    ll temp=people[2]*2+people[1];
+    cnt+=(temp/4);
+    if(temp%4)
+        cnt++;
+    cout<<cnt<<endl;
+    ////////////////////////
+    if(rt && file){
+     printf("\nTime taken: %.6fs", (double)(clock() - tStart)/CLOCKS_PER_SEC);
+    }
+    return 0;
+   }
+
+void FAST_IO()
+{
+    ios_base::sync_with_stdio(0);
+    //cin.tie(NULL);
+    //cout.tie(NULL);
+    //cout.setf(ios::fixed);
+    //cout.precision(2);
+    if(rt && file){ tStart = clock(); }
+    if(file){
+    #ifndef _offline
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+    }
+}

4b892b0cf3fd08a062c691b34ef74f681ec195ba
Saurav Paul <35382323+Saurav-Paul@users.noreply.github.com>
2019-03-23 23:21:28 +0600

    Brute_force Permutation

--- 1108C - Nice Garland.cpp
+++ 1108C - Nice Garland.cpp
@@ -113,3 +113,84 @@ void FAST_IO()
     #endif
     }
 }
+//////////////////////////////////////////////////////////////////////////
+/*                            or
+
+#include<bits/stdc++.h>
+#define endl '\n'
+#define ll long long int
+#define loop(i,a,b)           for(ll i=a;i<=b;++i)
+#define pb                    push_back
+#define F                     first
+#define S                     second
+#define mp                    make_pair
+#define clr(x)                x.clear()
+#define MOD                   1000000007
+#define itoc(c)               ((char)(((int)'0')+c))
+#define vl                    vector<ll>
+#define SZ(x)                 (x).size()
+#define all(p)                p.begin(),p.end()
+#define mid(s,e)              (s+(e-s)/2)
+#define sv()                  ll t,n; scanf("%lld",&t);n=t; while(t--)
+#define tcase()               ll t,n; cin>>t;n=t; while(t--)
+#define iscn(num)             scanf("%d",&num);
+using namespace std;
+typedef pair<ll,ll> Pair;
+bool file=0,rt=1;
+clock_t tStart ;
+void FAST_IO();
+////////////////////////
+
+int main()
+{
+
+    FAST_IO();
+    ////////////////////////
+    ll n;
+    string s,final,x;
+    cin>>n>>s;
+    x="RGB";
+    sort(all(x));
+    ll temp=INT_MAX;
+    do{
+        ll t=0;
+        for(int i=0; i<n ; i++){
+            if(s[i]!=x[i%3])
+                t++;
+        }
+        if(t<temp){
+            final=x;
+            temp=t;
+
+        }
+
+    }while(next_permutation(all(x)));
+    cout<<temp<<endl;
+    for(int i=0; i<n ; i++){
+        cout<<final[i%3];
+    }
+    cout<<endl;
+
+    ////////////////////////
+    if(rt && file){
+     printf("\nTime taken: %.6fs", (double)(clock() - tStart)/CLOCKS_PER_SEC);
+    }
+    return 0;
+   }
+
+void FAST_IO()
+{
+    ios_base::sync_with_stdio(0);
+    //cin.tie(NULL);
+    //cout.tie(NULL);
+    //cout.setf(ios::fixed);
+    //cout.precision(2);
+    if(rt && file){ tStart = clock(); }
+    if(file){
+    #ifndef _offline
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+    }
+}
+*/

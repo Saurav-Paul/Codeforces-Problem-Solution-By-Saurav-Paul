9e8ccfcb5faa821cf663dd4e5a6a7852d6257188
Saurav Paul <35382323+Saurav-Paul@users.noreply.github.com>
2019-04-11 20:45:49 +0600

    Math

---
+++ B. Messages.cpp
@@ -0,0 +1,75 @@
+#include<bits/stdc++.h>
+#define endl '\n'
+#define ll long long int
+#define loop(i,a,b)           for(ll i=a;i<=b;++i)
+#define pb                    push_back
+#define F                     first
+#define S                     second
+#define mp                    make_pair
+#define clr(x)                x.clear()
+#define MOD                   1000000007
+#define itoc(c)               ((char)(((int)'0')+c))
+#define vl                    vector<ll>
+#define SZ(x)                 (x).size()
+#define all(p)                p.begin(),p.end()
+#define mid(s,e)              (s+(e-s)/2)
+#define sv()                  ll t,n; scanf("%lld",&t);n=t; while(t--)
+#define tcase()               ll t,n; cin>>t;n=t; while(t--)
+#define iscn(num)             scanf("%d",&num);
+#define scn(num)              scanf("%lld ",&num);
+#define dbg(n)                cout<<(n)<<endl;
+using namespace std;
+const ll INF = 2e18 + 99;
+typedef pair<ll,ll> Pair;
+typedef vector<ll> vec;
+bool file=0,rt=1;
+clock_t tStart ;
+void FAST_IO();
+////////////////////////
+
+int main()
+{
+
+    FAST_IO();
+    ////////////////////////
+    ll n, a , b, c ,t ,k=0;
+    scanf("%lld %lld %lld %lld %lld",&n,&a,&b,&c,&t);
+    vector<ll> times(1100,0);
+    for(int i=0; i< n ; i++){
+        scn(k);
+        times[k]++;
+    }
+    k=0 ;
+    ll ans=n*a ,temp=0 ;
+    for(int i=1; i<= t ; i++){
+        temp-=(k*b);
+        temp+=(c*k);
+        if(times[i]){
+            k+=times[i];
+            temp+=(times[i]*a);
+        }
+
+    }
+    printf("%lld\n",max(ans, temp));
+    ////////////////////////
+    if(rt && file){
+     printf("\nTime taken: %.6fs", (double)(clock() - tStart)/CLOCKS_PER_SEC);
+    }
+    return 0;
+   }
+
+void FAST_IO()
+{
+    // ios_base::sync_with_stdio(0);
+    //cin.tie(NULL);
+    //cout.tie(NULL);
+    //cout.setf(ios::fixed);
+    //cout.precision(2);
+    if(rt && file){ tStart = clock(); }
+    if(file){
+    #ifndef _offline
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+    }
+}

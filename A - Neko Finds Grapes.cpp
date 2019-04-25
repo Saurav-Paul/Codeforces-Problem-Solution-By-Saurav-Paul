352fc21b82548b122d639a794e45a92e10cd1e3e
Saurav Paul <35382323+Saurav-Paul@users.noreply.github.com>
2019-04-25 20:20:10 +0600

    Implementation

---
+++ A - Neko Finds Grapes.cpp
@@ -0,0 +1,66 @@
+#include<bits/stdc++.h>
+#define endl '\n'
+#define ll long long int
+#define loop(i,a,b)           for(ll i=a;i<=b;++i)
+#define pb                    push_back
+#define F                     first
+#define S                     second
+#define mp                    make_pair
+#define clr(x)                x.clear()
+#define MOD                   1000000007
+#define itoc(c)               ((char)(((int)'0')+c))
+#define vl                    vector<ll>
+#define SZ(x)                 (x).size()
+#define all(p)                p.begin(),p.end()
+#define mid(s,e)              (s+(e-s)/2)
+#define sv()                  ll t,n; scanf("%lld",&t);n=t; while(t--)
+#define tcase()               ll t,n; cin>>t;n=t; while(t--)
+#define iscn(num)             scanf("%d",&num);
+#define scn(num)              scanf("%lld",&num);
+#define dbg(x)                cout <<"( "<<#x << " = " << x << " )"<<endl
+using namespace std;
+const ll INF = 2e18 + 99;
+typedef pair<ll,ll> Pair;
+typedef vector<ll> vec;;
+void FAST_IO();
+////////////////////////
+
+int main()
+{
+	ll n, m;
+	cin>>n>>m;
+	ll nodd=0,neven=0,modd=0,meven=0;
+	for(int i=0; i< n; i++){
+		ll temp;
+		cin>>temp;
+		if(temp&1){
+		nodd++;
+		}else{
+			neven++;
+		}
+
+	}
+	for(int i=0; i<m; i++){
+
+		ll temp; cin>>temp;
+		if(temp&1){
+			modd++;
+		}
+		else{
+
+			meven++;
+		}
+	}
+	cout<<min(neven,modd)+min(nodd,meven)<<endl;
+    return 0;
+}
+
+void FAST_IO()
+{
+    // ios_base::sync_with_stdio(0);
+    //cin.tie(NULL);
+    //cout.tie(NULL);
+    //cout.setf(ios::fixed);
+    //cout.precision(2);
+
+}

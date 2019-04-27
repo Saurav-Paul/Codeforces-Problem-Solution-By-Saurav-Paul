592ab95f427de3f09bc279c722daa19a1fb16a4b
Saurav Paul <35382323+Saurav-Paul@users.noreply.github.com>
2019-04-27 12:20:17 +0600

    Implementation

---
+++ A - Reachable Numbers.cpp
@@ -0,0 +1,53 @@
+#include<bits/stdc++.h>
+#define endl '\n'
+#define ll long long int
+#define loop(i,a,b)           for(ll i=a;i<=b;++i)
+#define pb                    push_back
+#define F                     first
+#define S                     second
+#define mp                    make_pair
+#define clr(x)                x.clear()
+#define MOD                   1000000007
+#define itoc(c)               ((char)(((int)'0')+c))
+#define vl                    vector<ll>
+#define SZ(x)                 (x).size()
+#define all(p)                p.begin(),p.end()
+#define mid(s,e)              (s+(e-s)/2)
+#define sv()                  ll t,n; scanf("%lld",&t);n=t; while(t--)
+#define tcase()               ll t,n; cin>>t;n=t; while(t--)
+#define iscn(num)             scanf("%d",&num);
+#define scn(num)              scanf("%lld",&num);
+#define dbg(x)                cout <<"( "<<#x << " = " << x << " )"<<endl
+using namespace std;
+const ll INF = 2e18 + 99;
+typedef pair<ll,ll> Pair;
+typedef vector<ll> vec;;
+void FAST_IO();
+////////////////////////
+
+int main()
+{
+	ll num; scanf("%lld",&num);
+	map<ll,ll> m;
+	m[num]=1;
+	while(1){
+	num++;
+		while(num%10==0)
+			num/=10;
+	if(m[num]==1)
+		break;
+	m[num]=1;	
+	}
+	cout<<m.size()<<endl;
+    return 0;
+}
+
+void FAST_IO()
+{
+    // ios_base::sync_with_stdio(0);
+    //cin.tie(NULL);
+    //cout.tie(NULL);
+    //cout.setf(ios::fixed);
+    //cout.precision(2);
+
+}

4513da768509b29d8d4e954b5d34eb197396bd39
Saurav Paul <35382323+Saurav-Paul@users.noreply.github.com>
2019-05-05 18:07:05 +0600

    Implementation

---
+++ 1162A - Zoning Restrictions Again.cpp
@@ -0,0 +1,55 @@
+#include<bits/stdc++.h>
+#define endl '\n'
+#define ll long long int
+#define loop(i,a,b)           for(ll i=a;i<=b;++i)
+#define pb                    push_back
+#define F                     first
+#define S                     second
+#define mp                    make_pair
+#define clr(x)                x.clear()
+#define MOD                   1000000007
+#define itoc(c)               ((char)(((int)'0')+c))
+#define vl                    vector<ll>
+#define SZ(x)                 (x).size()
+#define all(p)                p.begin(),p.end()
+#define mid(s,e)              (s+(e-s)/2)
+#define sv()                  ll t,n; scanf("%lld",&t);n=t; while(t--)
+#define tcase()               ll t,n; cin>>t;n=t; while(t--)
+#define iscn(num)             scanf("%d",&num);
+#define scn(num)              scanf("%lld",&num);
+#define dbg(x)                cout <<"( "<<#x << " = " << x << " )"<<endl
+using namespace std;
+const ll INF = 2e18 + 99;
+typedef pair<ll,ll> Pair;
+typedef vector<ll> vec;;
+void FAST_IO();
+////////////////////////
+
+int main()
+{
+	ll n,h,m;
+	scanf("%lld%lld%lld",&n,&h,&m);
+	vec spot(n+1,h);
+	ll total=0;
+	for(int i=0; i< m ; i++){
+		ll l,r,x;
+		scanf("%lld%lld%lld",&l,&r,&x);
+		for(ll j=l; j<=r; j++){
+			spot[j]=min(spot[j],x);
+		}
+	}
+	for(int i=1; i<=n; i++)
+		total+=(spot[i]*spot[i]);
+	printf("%lld\n",total);
+    return 0;
+}
+
+void FAST_IO()
+{
+    // ios_base::sync_with_stdio(0);
+    //cin.tie(NULL);
+    //cout.tie(NULL);
+    //cout.setf(ios::fixed);
+    //cout.precision(2);
+
+}

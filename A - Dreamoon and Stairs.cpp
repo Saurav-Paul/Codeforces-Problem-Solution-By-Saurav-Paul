7b6e99459a2c8a34e77b62435a212e58a611d690
Saurav Paul <35382323+Saurav-Paul@users.noreply.github.com>
2019-05-10 13:28:05 +0600

    Math Implementation

---
+++ A - Dreamoon and Stairs.cpp
@@ -0,0 +1,51 @@
+#include<bits/stdc++.h>
+#define endl '\n'
+#define ll long long int
+#define loop(i,a,b)           for(ll i=a;i<=b;++i)
+#define pb                    push_back
+#define F                     first
+#define S                     second
+#define mp                    make_pair
+#define clr(x)                x.clear()
+#define MOD                   1000000007
+#define itoc(c)               ((char)(((int)'0')+c))
+#define vl                    vector<ll>
+#define SZ(x)                 (x).size()
+#define all(p)                p.begin(),p.end()
+#define mid(s,e)              (s+(e-s)/2)
+#define sv()                  ll t,n; scanf("%lld",&t);n=t; while(t--)
+#define tcase()               ll t,n; cin>>t;n=t; while(t--)
+#define iscn(num)             scanf("%d",&num);
+#define scn(num)              scanf("%lld",&num);
+#define dbg(x)                cout <<"( "<<#x << " = " << x << " )"<<endl
+using namespace std;
+const ll INF = 2e18 + 99;
+typedef pair<ll,ll> Pair;
+typedef vector<ll> vec;;
+void FAST_IO();
+////////////////////////
+
+int main()
+{
+	ll n, m;
+	scanf("%lld%lld",&n,&m);
+	ll ans=n/2 + n%2; 
+	while(ans%m){
+		ans++;
+	}
+	if(ans>n)
+		puts("-1");
+	else
+	printf("%lld\n",ans);
+    return 0;
+}
+
+void FAST_IO()
+{
+    // ios_base::sync_with_stdio(0);
+    //cin.tie(NULL);
+    //cout.tie(NULL);
+    //cout.setf(ios::fixed);
+    //cout.precision(2);
+
+}

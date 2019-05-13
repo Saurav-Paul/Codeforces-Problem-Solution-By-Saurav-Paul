4f1d804972ba0ce54e4c3c033cfc217a960322d3
Saurav Paul <35382323+Saurav-Paul@users.noreply.github.com>
2019-05-13 21:25:20 +0600

    Implementation

---
+++ B - Expansion coefficient of the array.cpp
@@ -0,0 +1,65 @@
+#include<bits/stdc++.h>
+#define endl '\n'
+#define ll long long int
+#define loop(i,a,b)           for(ll i=a;i<=b;++i)
+#define pb                    push_back
+#define F                     first
+#define S                     second
+#define mp                    make_pair
+#define clr(x)                x.clear()
+#define MOD                   1000000007
+#define itoc(c)               ((char)(((int)'0')+c))
+#define vl                    vector<ll>
+#define SZ(x)                 (x).size()
+#define all(p)                p.begin(),p.end()
+#define mid(s,e)              (s+(e-s)/2)
+#define sv()                  ll t,n; scanf("%lld",&t);n=t; while(t--)
+#define tcase()               ll t,n; cin>>t;n=t; while(t--)
+#define iscn(num)             scanf("%d",&num);
+#define scn(num)              scanf("%lld",&num);
+#define dbg(x)                cout <<"( "<<#x << " = " << x << " )"<<endl
+using namespace std;
+const ll INF = 2e18 + 99;
+typedef pair<ll,ll> Pair;
+typedef vector<ll> vec;;
+void FAST_IO();
+void ara_read(vec &v,ll n);
+////////////////////////
+
+int main()
+{
+	ll n; scn(n);
+	vec ara;
+	ara_read(ara,n);
+	ll mini=INF;
+	for(int i=1; i< n; i++){
+		ll temp=min(ara[i],ara[0]);
+		temp/=i;
+		mini=min(mini,temp);
+	}
+	for(ll i=n-2; i>=0; i--){
+		ll temp=min(ara[i],ara[n-1]);
+		temp/=(n-1-i);
+		mini=min(mini,temp);
+	}
+	cout<<mini<<endl;
+   return 0;
+}
+
+void FAST_IO()
+{
+    // ios_base::sync_with_stdio(0);
+    //cin.tie(NULL);
+    //cout.tie(NULL);
+    //cout.setf(ios::fixed);
+    //cout.precision(2);
+
+}
+void ara_read(vec &v,ll n){
+	ll temp;
+	for(ll i=0; i<n; i++){
+		scanf("%lld",&temp);
+		v.emplace_back(temp);
+	}
+	
+}

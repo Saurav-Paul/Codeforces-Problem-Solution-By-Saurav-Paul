0dbb6e5456be3324f8ed4f82a3312ea46c6a4d49
Saurav Paul <35382323+Saurav-Paul@users.noreply.github.com>
2019-05-16 20:28:52 +0600

    Implementation

---
+++ A - Telephone Number.cpp
@@ -0,0 +1,76 @@
+#include<bits/stdc++.h>
+#define endl '\n'
+#define ll long long int
+#define loop(i,a,b)           for(ll i=a;i<=b;++i)
+#define pb                    push_back
+#define F                     first
+#define S                     second
+#define mp                    make_pair
+#define clr(x)                x.clear()
+#define MOD                   1000000007
+#define itoc(c)               ((char)(((int)'0')+c))
+#define vl                    vector<ll>
+#define SZ(x)                 (x).size()
+#define all(p)                p.begin(),p.end()
+#define mid(s,e)              (s+(e-s)/2)
+#define sv()                  ll t,n; scanf("%lld",&t);n=t; while(t--)
+#define tcase()               ll t,n; cin>>t;n=t; while(t--)
+#define iscn(num)             scanf("%d",&num);
+#define scn(num)              scanf("%lld",&num);
+#define dbg(x)                cout <<"( "<<#x << " = " << x << " )"<<endl
+using namespace std;
+const ll INF = 2e18 + 99;
+typedef pair<ll,ll> Pair;
+typedef vector<ll> vec;;
+void FAST_IO();
+void ara_read(vec &v,ll n);
+////////////////////////
+
+int main()
+{
+	ll tcase; scn(tcase);
+	while(tcase--){
+		bool flag=true;
+		ll n; scn(n);
+		string s; cin>>s;
+		if(n<11){
+			flag=false;
+		}else if(n==11 && s[0]!='8'){
+			flag=false;
+		}else{
+			ll limit=n-11;
+			flag = false;
+			for(int i=0; i<= limit ; i++){
+				if(s[i]=='8'){
+					flag=true;
+					break;
+				}
+			}
+				
+		}
+		if(flag)
+			puts("YES");
+		else
+			puts("NO");
+		
+	}
+   return 0;
+}
+
+void FAST_IO()
+{
+    // ios_base::sync_with_stdio(0);
+    //cin.tie(NULL);
+    //cout.tie(NULL);
+    //cout.setf(ios::fixed);
+    //cout.precision(2);
+
+}
+void ara_read(vec &v,ll n){
+	ll temp;
+	for(ll i=0; i<n; i++){
+		scanf("%lld",&temp);
+		v.emplace_back(temp);
+	}
+	
+}

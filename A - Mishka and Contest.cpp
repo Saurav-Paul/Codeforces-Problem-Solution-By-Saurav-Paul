91bbc9a4929f63f603bc2ce13e1c6e6e93ef28ea
Saurav Paul <35382323+Saurav-Paul@users.noreply.github.com>
2019-05-19 12:03:38 +0600

    Greedy

---
+++ A - Mishka and Contest.cpp
@@ -0,0 +1,75 @@
+#include<bits/stdc++.h>
+#define endl '\n'
+#define ll long long int
+#define loop(i,a,b)           for(ll i=a;i<=b;++i)
+#define pb                    push_back
+#define F                     first
+#define S                     second
+#define mp                    make_pair
+#define clr(x)                x.clear()
+#define MOD                   1000000007
+#define itoc(c)               ((char)(((int)'0')+c))
+#define vl                    vector<ll>
+#define SZ(x)                 (x).size()
+#define all(p)                p.begin(),p.end()
+#define mid(s,e)              (s+(e-s)/2)
+#define sv()                  ll t,n; scanf("%lld",&t);n=t; while(t--)
+#define tcase()               ll t,n; cin>>t;n=t; while(t--)
+#define iscn(num)             scanf("%d",&num);
+#define scn(num)              scanf("%lld",&num);
+#define dbg(x)                cout <<"( "<<#x << " = " << x << " )"<<endl
+using namespace std;
+const ll INF = 2e18 + 99;
+typedef pair<ll,ll> Pair;
+typedef vector<ll> vec;;
+void FAST_IO();
+void ara_read(vec &v,ll n);
+////////////////////////
+
+int main()
+{
+	ll n, k;
+	cin>>n>>k;
+	vector<ll> num;
+	for(int i=0; i< n; i++){
+		int temp;
+		cin>> temp;
+		num.push_back(temp);
+	}
+	int i=0, j=n-1;
+	ll ans=0;
+	while(i<=j){
+		if(num[i]<=k){
+			ans++;
+			i++;
+		}else if(num[j]<=k){
+			ans++;
+			j--;
+		}
+		else{
+			break ;
+		}
+		
+	}
+	cout<<ans<<endl;
+	
+   return 0;
+}
+
+void FAST_IO()
+{
+    // ios_base::sync_with_stdio(0);
+    //cin.tie(NULL);
+    //cout.tie(NULL);
+    //cout.setf(ios::fixed);
+    //cout.precision(2);
+
+}
+void ara_read(vec &v,ll n){
+	ll temp;
+	for(ll i=0; i<n; i++){
+		scanf("%lld",&temp);
+		v.emplace_back(temp);
+	}
+	
+}

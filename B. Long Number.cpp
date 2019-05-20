251043f0e3c6e33e037c7b72aa44cb5e2e99f7dd
Saurav Paul <35382323+Saurav-Paul@users.noreply.github.com>
2019-05-20 23:40:56 +0600

    String Implementation

---
+++ B. Long Number.cpp
@@ -0,0 +1,78 @@
+#include<bits/stdc++.h>
+#define endl '\n'
+#define ll long long int
+#define loop(i,a,b)           for(ll i=a;i<=b;++i)
+#define pb                    push_back
+#define F                     first
+#define S                     second
+#define mp                    make_pair
+#define clr(x)                x.clear()
+#define MOD                   1000000007
+#define itoc(c)               ((char)(((int)'0')+c))
+#define vl                    vector<ll>
+#define SZ(x)                 (x).size()
+#define all(p)                p.begin(),p.end()
+#define mid(s,e)              (s+(e-s)/2)
+#define sv()                  ll t,n; scanf("%lld",&t);n=t; while(t--)
+#define tcase()               ll t,n; cin>>t;n=t; while(t--)
+#define iscn(num)             scanf("%d",&num);
+#define scn(num)              scanf("%lld",&num);
+#define scn2(num,num2)        scanf("%lld%lld",&num,&num2);
+#define dbg(x)                cerr <<"( "<<#x << " = " << x << " )"<<endl
+using namespace std;
+const ll INF = 2e18 + 99;
+typedef pair<ll,ll> Pair;
+typedef vector<ll> vec;;
+void FAST_IO();
+void ara_read(vec &v,ll n);
+////////////////////////
+
+int main()
+{
+	ll n; scanf("%lld",&n);
+	string s; cin>>s;
+	vec num(10);
+	// taking input...
+	for(int i=1 ; i<= 9 ; ++i){
+		ll temp; scn(temp);
+		num[i]=temp;
+	}
+	// flag for remembering if visited...
+	bool flag=false;
+	for(int i=0; i< n; i++){
+		ll temp= num[s[i]-'0'];
+		
+		if((s[i]-'0')<temp){
+			// let's change some contigeous number ...
+			s[i]='0'+temp ;
+			flag= true;
+			
+		}else if(flag && (s[i]-'0'==temp) ){
+			continue ;
+		}
+		else{
+			if(flag)
+				break ;
+		}
+	}
+	cout<<s<<endl;
+    return 0;
+}
+
+void FAST_IO()
+{
+    // ios_base::sync_with_stdio(0);
+    //cin.tie(NULL);
+    //cout.tie(NULL);
+    //cout.setf(ios::fixed);
+    //cout.precision(2);
+
+}
+void ara_read(vec &v,ll n){
+	ll temp;
+	for(ll i=0; i<n; i++){
+		scanf("%lld",&temp);
+		v.emplace_back(temp);
+	}
+	
+}

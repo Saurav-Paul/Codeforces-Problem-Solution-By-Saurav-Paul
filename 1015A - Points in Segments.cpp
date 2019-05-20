1b8d8e23091d21f655daa3abdd88548b8408b665
Saurav Paul <35382323+Saurav-Paul@users.noreply.github.com>
2019-05-20 14:10:39 +0600

    Implementation

---
+++ 1015A - Points in Segments.cpp
@@ -0,0 +1,74 @@
+#include<bits/stdc++.h>
+#define endl '\n'
+#define ll long long int
+#define loop(i,a,b)           for(ll i=a;i<=b;++i)
+#define pb                    push_back
+#define F                     first
+#define S                     second
+#define mp                    make_pair
+#define clr(x)                x.clear()
+#define MOD                   1000000007
+#define itoc(c)               ((char)(((int)'0')+c))
+#define vl                    vector<ll>
+#define SZ(x)                 (x).size()
+#define all(p)                p.begin(),p.end()
+#define mid(s,e)              (s+(e-s)/2)
+#define sv()                  ll t,n; scanf("%lld",&t);n=t; while(t--)
+#define tcase()               ll t,n; cin>>t;n=t; while(t--)
+#define iscn(num)             scanf("%d",&num);
+#define scn(num)              scanf("%lld",&num);
+#define scn2(num,num2)        scanf("%lld%lld",&num,&num2);
+#define dbg(x)                cerr <<"( "<<#x << " = " << x << " )"<<endl
+using namespace std;
+const ll INF = 2e18 + 99;
+typedef pair<ll,ll> Pair;
+typedef vector<ll> vec;;
+void FAST_IO();
+void ara_read(vec &v,ll n);
+////////////////////////
+
+int main()
+{
+	ll n, m;
+	scanf("%lld%lld",&n,&m);
+	vector<bool> segment(m+1,0);
+	ll cnt=0;
+	for(int i=0; i< n; i++){
+		int x, y;
+		scanf("%d%d",&x,&y);
+		if(x>y)
+			swap(x,y);
+		for(int k=x; k<=y; k++){
+			if(!segment[k]){
+				cnt++;
+				segment[k]=true;
+			}
+		}
+	}
+	cnt=m-cnt;
+	printf("%lld\n",cnt);
+	for(int i=1; i<= m; i++){
+		if(!segment[i])
+			printf("%d ",i);
+	}
+	
+    return 0;
+}
+
+void FAST_IO()
+{
+    // ios_base::sync_with_stdio(0);
+    //cin.tie(NULL);
+    //cout.tie(NULL);
+    //cout.setf(ios::fixed);
+    //cout.precision(2);
+
+}
+void ara_read(vec &v,ll n){
+	ll temp;
+	for(ll i=0; i<n; i++){
+		scanf("%lld",&temp);
+		v.emplace_back(temp);
+	}
+	
+}

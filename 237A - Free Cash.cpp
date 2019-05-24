ac2526c83ab1d8c6a4a9796465ac39c6b649c204
Saurav Paul <35382323+Saurav-Paul@users.noreply.github.com>
2019-05-24 17:46:24 +0600

    Implementation

---
+++ 237A - Free Cash.cpp
@@ -0,0 +1,60 @@
+#include<bits/stdc++.h>
+#define endl '\n'
+#define ll long long int
+#define loop(i,a,b)           for(ll i=a;i<=b;++i)
+#define boost				  ios_base::sync_with_stdio(0);cin.tie(NULL);
+#define pb                    push_back
+#define F                     first
+#define S                     second
+#define mp                    make_pair
+#define clr(x)                x.clear()
+#define MOD                   1e9+7
+#define itoc(c)               ((char)(((int)'0')+c))
+#define all(p)                p.begin(),p.end()
+#define iscn(num)             scanf("%d",&num);
+#define scn(num)              scanf("%lld",&num);
+#define scn2(num,num2)        scanf("%lld%lld",&num,&num2);
+#define dbg(x)                cerr <<"( "<<#x << " = " << x << " )"<<endl
+using namespace std;
+template<class T >T set_bit(T N,T pos){return N=N | (1<<pos);}
+template<class T >T reset_bit(T N,T pos){return N= N & ~(1<<pos);}
+template<class T > bool check_bit(T N,T pos){return (bool)(N & (1<<pos));}
+template <class T > void max_self(T &a, T b){
+	a=max(a,b);
+}
+template <class T > void min_self(T &a, T b){
+	a=min(a,b);
+}
+////////////////////////
+
+const ll INF = 2e18 + 99;
+typedef pair<ll,ll> Pair;
+typedef vector<int> vec;;
+
+void ara_read(vec &v,ll n){
+	ll temp;
+	for(ll i=0; i< n; i++){
+		scanf("%lld",&temp);
+		v.emplace_back(temp);
+	}
+}
+void ara_print(vec &v){
+	for(ll x : v)
+		printf("%lld ",x);
+	puts("");
+}
+////////////////////////
+int rem[25][70];
+int main()
+{
+	int n; scanf("%d",&n);
+	int maximum=0;
+	while(n--){
+		int hr,minute;
+		scanf("%d%d",&hr,&minute);
+		rem[hr][minute]++;
+		max_self(maximum,rem[hr][minute]);
+	}
+	printf("%d\n",maximum);
+    return 0;
+}

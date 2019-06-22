a96dcd6f585027e97a50cd270613d051188bd17f
Saurav Paul <35382323+Saurav-Paul@users.noreply.github.com>
2019-06-22 09:55:59 +0600

    Math

---
+++ A - Alex and a Rhombus.cpp
@@ -0,0 +1,28 @@
+
+#include<bits/stdc++.h>
+#define endl '\n'
+#define ll long long int
+#define loop(i,a,b)           for(ll i=a;i<=b;++i)
+#define boost		          ios_base::sync_with_stdio(0);
+#define eb                    emplace_back
+#define mp                    make_pair
+#define clr(x)                x.clear()
+#define MOD                   1e9+7
+#define itoc(c)               ((char)(((int)'0')+c))
+#define ctoi(c)               ((int)(((int)c)-'0'))
+#define all(p)                p.begin(),p.end()
+#define iscn(num)             scanf("%d",&num);
+#define scn(num)              scanf("%lld",&num);
+#define scn2(num,num2)        scanf("%lld%lld",&num,&num2);
+using namespace std;
+
+
+int main()
+{
+	int n;
+	cin >> n;
+	ll ans =  n*n + (n-1) * (n-1) ;
+	cout << ans << endl;
+	
+    return 0;
+}

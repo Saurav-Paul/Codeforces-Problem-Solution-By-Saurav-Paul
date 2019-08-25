e3b1170cf270b666c0e75c660da0fa8a63eafaea
Saurav Paul <35382323+Saurav-Paul@users.noreply.github.com>
2019-08-25 23:54:01 +0600

    Math

---
+++ 914A - Perfect Squares.cpp
@@ -0,0 +1,106 @@
+				/*Saurav Paul*/
+#pragma GCC optimize("O3")
+#include<bits/stdc++.h>
+#define loop(i,a,b)           for(ll i=a;i<=b;++i)
+#define boost		          ios_base::sync_with_stdio(0);
+#define eb                    emplace_back
+#define itoc(c)               ((char)(((int)'0')+c))
+#define ctoi(c)               ((int)(((int)c)-'0'))
+#define all(p)                p.begin(),p.end()
+
+using ll = long long int ;
+using namespace std;
+///////////////////////
+
+#define sim template < class c
+#define ris return * this
+#define dor > debug & operator <<
+#define eni(x) sim > typename \
+enable_if<sizeof dud<c>(0) x 1, debug&>::type operator<<(c i) {
+sim > struct rge { c b, e; };
+sim > rge<c> range(c i, c j) { return rge<c>{i, j}; }
+sim > auto dud(c* x) -> decltype(cerr << *x, 0);
+sim > char dud(...);
+struct debug {
+#ifdef PAUL
+~debug() { cerr << endl; }
+eni(!=) cerr << boolalpha << i; ris; }
+eni(==) ris << range(begin(i), end(i)); }
+sim, class b dor(pair < b, c > d) {
+  ris << "(" << d.first << ", " << d.second << ")";
+}
+sim dor(rge<c> d) {
+  *this << "[";
+  for (auto it = d.b; it != d.e; ++it)
+    *this << ", " + 2 * (it == d.b) << *it;
+  ris << "]";
+}
+#else
+sim dor(const c&) { ris; }
+#endif
+};
+#define dbg(...) " [" << #__VA_ARGS__ ": " << (__VA_ARGS__) << "] "
+
+#ifdef ONLINE_JUDGE
+#define cerr if (false) cerr
+#endif
+void time(){
+	#ifdef PAUL
+		cerr << "\nTime elapsed: " << 1.0 * clock() / CLOCKS_PER_SEC << " s.\n";
+	#endif
+}
+///////////////////////
+/*
+string readString(){
+    string s;
+    char c=getchar();
+    while(isspace(c)){
+        c=getchar();
+    }
+    while(!isspace(c)){
+        s+=c;
+        c=getchar();
+    }
+    return s;
+}
+*/
+const int INF = 1e9 + 99;
+using Pair = pair<int,int >;
+using vec = vector<int> ;
+
+void vecread(vec &v){
+	for(auto &x : v){
+		scanf("%d",&x);
+	}
+}
+void vecprint(vec &v){
+	for(auto x : v){
+		printf("%d ",x);
+	} puts("");
+}
+
+
+int main()
+{
+	int n;
+	scanf("%d",&n);
+	
+	vector<int> v(n);
+	vecread(v);
+	sort(all(v));
+	int ans = 0;
+	const int nax = 1e6 + 90;
+	bool check[nax]={};
+	
+	for(int i = 0 ; i*i < nax ; i++)
+		check[i*i] = true;
+	
+	for(int i =0 ; i < n; i++){
+		if(v[i] < 0 or check[v[i]]==false)
+			ans = v[i];
+	}
+	
+	printf("%d\n",ans);
+	time();
+    return 0;
+}

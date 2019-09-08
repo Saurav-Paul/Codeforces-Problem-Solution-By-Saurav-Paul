87016e9f7a7e05443aa2b4abf55644e84660b1ed
Saurav Paul <35382323+Saurav-Paul@users.noreply.github.com>
2019-09-08 20:47:31 +0600

    Number_theory

---
+++ 26A - Almost Prime.cpp
@@ -0,0 +1,95 @@
+				/*Saurav Paul*/
+#pragma GCC optimize("O3")
+#include<bits/stdc++.h>
+#define loop(i,a,b)           for(ll i=a;i<=b;++i)
+#define boost		          ios_base::sync_with_stdio(0);
+#define eb                    emplace_back
+#define itoc(c)               ((char)(((int)'0')+c))
+#define ctoi(c)               ((int)(((int)c)-'0'))
+#define all(p)                p.begin(),p.end()
+#define bug()				  debug() <<
+
+using namespace std;
+///////////////////////
+
+#define sim template < class c
+#define ris return * this
+#define dor > debug & operator <<
+#define eni(x) sim > typename \
+enable_if<sizeof dud<c>(0) x 1, debug&>::type operator<<(c i) {
+sim > struct rge { c b, e; };
+sim > rge<c> range(c i, c j) { return rge<c>{i, j}; }
+sim > auto dud(c* x) -> decltype(cerr << *x, 0);
+sim > char dud(...);
+struct debug {
+#ifdef PAUL
+~debug() { cerr << endl; }
+eni(!=) cerr << boolalpha << i; ris; }
+eni(==) ris << range(begin(i), end(i)); }
+sim, class b dor(pair < b, c > d) {
+  ris << "(" << d.first << ", " << d.second << ")";
+}
+sim dor(rge<c> d) {
+  *this << "[";
+  for (auto it = d.b; it != d.e; ++it)
+    *this << ", " + 2 * (it == d.b) << *it;
+  ris << "]";
+}
+#else
+sim dor(const c&) { ris; }
+#endif
+};
+#define dbg(...) " [" << #__VA_ARGS__ ": " << (__VA_ARGS__) << "] "
+
+#ifdef ONLINE_JUDGE
+#define cerr if (false) cerr
+#endif
+void time(){
+	#ifdef PAUL
+		cerr << "\nTime elapsed: " << 1.0 * clock() / CLOCKS_PER_SEC << " s.\n";
+	#endif
+}
+///////////////////////
+const int INF = 1e9 + 99;
+using ll = long long int ;
+using pii = pair<int,int >;
+using vec = vector<int> ;
+vec primes;
+void sieve(int n){
+	bool notprime[n]={};
+	primes.eb(2);
+	
+	for(int i = 3 ; i <= n ; i+=2){
+		if(notprime[i]==false){
+			primes.eb(i);
+			for(int j = i+ (i<<1) ; j <= n ; j+= (i<<1)){
+				notprime[j] = true;
+			}
+		}
+	}
+}
+
+int main()
+{
+	int n;
+	cin >> n;
+	
+	sieve(3000+10);
+	
+	int cnt[n+10]={};
+	
+	for(auto x : primes){
+		for(int i = x ; i <= n ; i += x)
+			cnt[i] ++;
+	}
+	
+	int ans = 0;
+	
+	for(int i = 0 ; i <= n; i++)
+		if(cnt[i]==2)
+			ans ++;
+			
+	printf("%d\n",ans);
+	time();
+    return 0;
+}

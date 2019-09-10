272b35143484b36e3fd46fbc10625993e3060b23
Saurav Paul <35382323+Saurav-Paul@users.noreply.github.com>
2019-09-10 11:54:27 +0600

    Math

---
+++ 166E - Tetrahedron.cpp
@@ -0,0 +1,84 @@
+				/*Saurav Paul*/
+#pragma GCC optimize("O3")
+#include<bits/stdc++.h>
+#define loop(i,a,b)           for(ll i=a;i<=b;++i)
+#define boost		          ios_base::sync_with_stdio(0);
+#define eb                    emplace_back
+#define itoc(c)               ((char)(((int)'0')+c))
+#define ctoi(c)               ((int)(((int)c)-'0'))
+#define all(p)                p.begin(),p.end()
+#define bug()				  debug() <<
+
+using namespace std;
+///////////////////////
+
+#define sim template < class c
+#define ris return * this
+#define dor > debug & operator <<
+#define eni(x) sim > typename \
+enable_if<sizeof dud<c>(0) x 1, debug&>::type operator<<(c i) {
+sim > struct rge { c b, e; };
+sim > rge<c> range(c i, c j) { return rge<c>{i, j}; }
+sim > auto dud(c* x) -> decltype(cerr << *x, 0);
+sim > char dud(...);
+struct debug {
+#ifdef PAUL
+~debug() { cerr << endl; }
+eni(!=) cerr << boolalpha << i; ris; }
+eni(==) ris << range(begin(i), end(i)); }
+sim, class b dor(pair < b, c > d) {
+  ris << "(" << d.first << ", " << d.second << ")";
+}
+sim dor(rge<c> d) {
+  *this << "[";
+  for (auto it = d.b; it != d.e; ++it)
+    *this << ", " + 2 * (it == d.b) << *it;
+  ris << "]";
+}
+#else
+sim dor(const c&) { ris; }
+#endif
+};
+#define dbg(...) " [" << #__VA_ARGS__ ": " << (__VA_ARGS__) << "] "
+
+#ifdef ONLINE_JUDGE
+#define cerr if (false) cerr
+#endif
+void time(){
+	#ifdef PAUL
+		cerr << "\nTime elapsed: " << 1.0 * clock() / CLOCKS_PER_SEC << " s.\n";
+	#endif
+}
+///////////////////////
+const int INF = 1e9 + 99;
+using ll = long long int ;
+using pii = pair<int,int >;
+using vec = vector<int> ;
+const int mod = 1000000007;
+
+inline int add (int x,int y ,int z){
+	return (x%mod + (y%mod + z%mod)%mod )%mod;
+}
+
+int idea(int n){
+	int a , b, c, d;
+	a=b=c=0 , d = 1;
+	
+	for(int i = 0 ; i < n; i++){
+		int aa = add(b,c,d);
+		int bb = add(a,c,d);
+		int cc = add(a,b,d);
+		int dd = add(a,b,c);
+		a = aa , b = bb , c = cc , d = dd;
+	}
+	return d;
+}
+
+int main()
+{
+	boost;
+	int n; cin >> n;
+	cout << idea(n) << endl;
+	time();
+    return 0;
+}

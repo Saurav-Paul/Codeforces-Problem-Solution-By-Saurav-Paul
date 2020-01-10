bfd8303e66eb8fedae46f31e4778231a323e1628
Saurav Paul <35382323+Saurav-Paul@users.noreply.github.com>
2020-01-11 00:09:45 +0600

    Implementation

---
+++ A - Mezo Playing Zoma .cpp
@@ -0,0 +1,20 @@
+
+/**
+ *    author:  Saurav_Paul 
+ *    created: 10.01.2020 20:05:58       
+**/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int main()
+{
+	int n ;
+	cin >> n;
+	string s; 
+	cin >> s;
+	cout << n + 1 << endl;
+
+    return 0;
+}

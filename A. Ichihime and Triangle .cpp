bc470bff19e3a0c655756dbb3c82cd4ea865d633
Saurav Paul <35382323+Saurav-Paul@users.noreply.github.com>
2020-04-16 20:59:28 +0600

    Math

---
+++ A. Ichihime and Triangle .cpp
@@ -0,0 +1,29 @@
+
+/**
+ *    author:  Saurav_Paul 
+ *    created: 15.04.2020 20:42:14       
+**/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+void solve() {
+
+    long long a , b , c, d;
+    cin >> a >> b >> c >> d;
+    cout << b << " " << c << " " << c << endl ;
+}
+
+int main(){
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    
+    int testcase;
+    cin >> testcase;
+    for (int t = 1; t <= testcase; t++){
+        solve();
+    }
+
+    return 0;
+}

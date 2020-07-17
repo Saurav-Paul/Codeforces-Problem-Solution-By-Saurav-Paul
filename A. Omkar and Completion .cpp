fc14cd98adfab180c4a935fa292a7dcf34d0eecf
Saurav Paul <35382323+Saurav-Paul@users.noreply.github.com>
2020-07-17 20:13:49 +0600

    Create A. Omkar and Completion .cpp

---
+++ A. Omkar and Completion .cpp
@@ -0,0 +1,30 @@
+
+/**
+ *    author        : Saurav Paul 
+ *    created       : July 17, 2020 7:58 PM
+ *    Problem Name  : A. Omkar and Completion
+ *    Problem Limit : 1000 ms , 256 MB
+ *    Problem Url   : https://codeforces.com/contest/1372/problem/A
+**/
+
+#include<bits/stdc++.h>
+using namespace std;
+void solve(){
+    int n;
+    cin >> n;
+    for(int i = 0 ; i < n; i++ ){
+        cout << 1 << " ";
+    }
+    cout << endl ;
+}
+int main(){
+    ios_base::sync_with_stdio(false);
+
+    int testcase ;
+    cin >> testcase ;
+    for(int i = 0 ; i < testcase ; i ++){
+        solve();
+    }
+
+    return 0 ;
+}

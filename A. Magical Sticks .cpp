44cb8ba2a9f3bcbf4a8d8366af8e77603d227c68
Saurav Paul <35382323+Saurav-Paul@users.noreply.github.com>
2020-08-03 20:48:25 +0600

    Create A. Magical Sticks .cpp

---
+++ A. Magical Sticks .cpp
@@ -0,0 +1,31 @@
+
+/**
+ *    author        : Saurav Paul 
+ *    created       : August 03, 2020 8:16 PM
+ *    Problem Name  : A. Magical Sticks
+ *    Problem Limit : 1000 ms , 256 MB
+ *    Problem Url   : https://codeforces.com/contest/1371/problem/A
+ *    @genarated by : ai-virtual-assistant
+**/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+void solve(){
+
+    int n ;
+    cin >> n;
+    cout << ((n+1) >> 1) << endl ;
+}
+
+int main(){
+    ios_base::sync_with_stdio(false);
+
+    int testcase ;
+    cin >> testcase ;
+    for(int i = 0 ; i < testcase ; i ++){
+        solve();
+    }
+
+    return 0 ;
+}

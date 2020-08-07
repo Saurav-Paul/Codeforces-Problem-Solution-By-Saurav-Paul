976a1a6b3e184d2a4764e2ff98cae2624c9e0efe
Saurav Paul <35382323+Saurav-Paul@users.noreply.github.com>
2020-08-08 00:12:05 +0600

    Create A. Rainbow Dash, Fluttershy and Chess Coloring .cpp

---
+++ A. Rainbow Dash, Fluttershy and Chess Coloring .cpp
@@ -0,0 +1,31 @@
+
+/**
+ *    author        : Saurav Paul 
+ *    created       : August 07, 2020 8:35 PM
+ *    Problem Name  : A. Rainbow Dash, Fluttershy and Chess Coloring
+ *    Problem Limit : 1000 ms , 256 MB
+ *    Problem Url   : https://codeforces.com/contest/1393/problem/a
+ *    @genarated by : ai-virtual-assistant
+**/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+void solve(){
+    long long n ;
+    cin >> n;
+    cout << (n/2)+1 << endl ;
+}
+
+int main(){
+    ios_base::sync_with_stdio(false);
+
+    int testcase ;
+    cin >> testcase ;
+    for(int i = 0 ; i < testcase ; i ++){
+        solve();
+    }
+
+
+    return 0 ;
+}

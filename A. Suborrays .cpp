3674855f1046db10007f205efd7d6ebe1798fe28
Saurav Paul <35382323+Saurav-Paul@users.noreply.github.com>
2020-08-10 11:55:21 +0600

    Create A. Suborrays .cpp

---
+++ A. Suborrays .cpp
@@ -0,0 +1,35 @@
+
+/**
+ *    author        : Saurav Paul 
+ *    created       : August 09, 2020 8:35 PM
+ *    Problem Name  : A. Suborrays
+ *    Problem Limit : 1000 ms , 256 MB
+ *    Problem Url   : https://codeforces.com/contest/1391/problem/0
+ *    @genarated by : ai-virtual-assistant
+**/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+void solve(){
+
+    int n;
+    cin >> n;
+    for(int i = 1 ; i <= n ; i++){
+        cout << i << " " ;
+    }
+    cout << endl;
+}
+
+int main(){
+    ios_base::sync_with_stdio(false);
+
+
+    int testcase ;
+    cin >> testcase ;
+    for(int i = 0 ; i < testcase ; i ++){
+        solve() ;
+    }
+
+    return 0 ;
+}

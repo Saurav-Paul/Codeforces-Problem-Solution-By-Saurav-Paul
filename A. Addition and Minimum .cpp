181e76b1391dd1d1e7ad1e33909c93b40d1c5f97
Saurav Paul <35382323+Saurav-Paul@users.noreply.github.com>
2020-09-29 22:59:40 +0600

    Create A. Addition and Minimum .cpp

---
+++ A. Addition and Minimum .cpp
@@ -0,0 +1,118 @@
+
+/**
+ *    author        : Saurav-Paul 
+ *    created       : September 29, 2020 9:23 PM
+ *    Problem Name  : A. Addition and Minimum
+ *    Problem Url   : https://codeforces.com/edu/course/2/lesson/5/2/practice/contest/279653/problem/A
+ *    @genarated by : ai-virtual-assistant
+**/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+using ll = long long ;
+
+class Segment_tree{
+
+    struct data{
+        ll prop , mn ;
+        data(ll _val){
+            prop = 0 ;
+            mn = _val ;
+        }
+    };
+    public:
+    vector<data> tree ;
+    const ll INF = 1e18 + 90 ;
+
+    Segment_tree(int n){
+        int size = 1 ;
+        while(size < n){
+            size *= 2 ;
+        }
+        tree.assign(size*2,data(0)) ;
+    }
+
+    ll query(int node, int l, int r, int st, int sp){
+        if(st > r || sp < l) return INF ;
+        else if(st >= l && sp <=r ){
+            return tree[node].mn ;
+        }
+        else{
+            int lt = (node << 1) ;
+            int rt = lt + 1;
+            int mid = st + ((sp-st)>>1) ;
+
+            ll q1 = query(lt,l,r,st,mid) ;
+            ll q2 = query(rt,l,r,mid+1,sp) ;
+
+            return min(q1,q2) + tree[node].prop ;
+
+        }
+    }
+
+    void update(int node , int l , int r, int st , int sp , int val){
+        if(st > r || sp < l) return ;
+        if(st >= l && sp <= r){
+            tree[node].prop += val ;
+            tree[node].mn += val ;
+        }
+        else{
+            int lt = (node << 1) ;
+            int rt = lt + 1;
+            int mid = st + ((sp-st)>>1) ;
+
+            update(lt,l,r,st,mid,val) ;
+            update(rt,l,r,mid+1,sp,val) ;
+
+            tree[node].mn = min(tree[lt].mn,tree[rt].mn) + tree[node].prop ;
+
+        }
+
+    }
+
+
+
+};
+
+
+class TC{
+
+    public :
+    void solve(){
+        int n , m;
+        cin >> n >> m ;
+
+        Segment_tree Tree(n) ;
+
+        for(int i = 1; i <= m; i++){
+            int op ; cin >> op ;
+            if(op == 1){
+                int l , r , val ;
+                cin >> l >> r >> val ;
+                -- r;
+                Tree.update(1,l,r,0,n-1,val) ;
+            }
+            else if(op == 2){
+                int l , r ;
+                cin >> l >> r;
+                -- r;
+                cout << Tree.query(1,l,r,0,n-1) << endl ;
+            }
+            else assert(false) ;
+        }
+
+    }
+};
+
+int main(){
+    ios_base::sync_with_stdio(false);
+
+    int testcases = 1;
+    for(int i = 1 ; i <= testcases ; i ++){
+        TC tc;
+        tc.solve() ;
+    }
+
+    return 0 ;
+}
